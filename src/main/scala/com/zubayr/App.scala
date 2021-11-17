5f3c8c779602a7f4bf293ec93ef073fe9bc1a3f5
zubayrrr <31969517+zubayrrr@users.noreply.github.com>
2021-11-18 03:00:51 +0530

    🔥 Initial commit 🔥

---
+++ src/main/scala/com/zubayr/App.scala
@@ -0,0 +1,21 @@
+package com.zubayr
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .getOrCreate()
+
+    val data = spark.sparkContext.parallelize(Seq("Hello World", "This is some text", "Hello text"))
+    val map = data.flatMap(e => e.split(" ")).map(word => (word, 1))
+    val counts = map.reduceByKey(_ + _).repartition(1)
+
+    counts.saveAsTextFile("wordcount")
+
+  }
+}
